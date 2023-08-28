99327bac1ca798c0932fcde41a8acf3bebfa1076
Ayşe Nur Çuhacı <109754763+cuhaciayse@users.noreply.github.com>
2023-08-28 17:01:38 +0300

    Add project files.

---
+++ RealEstate_Dapper_Api/Models/DapperContext/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace RealEstate_Dapper_Api.Models.DapperContext
+{
+    public class Context
+    {
+        private readonly string _connectionString;
+        private readonly IConfiguration _configuration;
+
+        public Context(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("connection");
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+
+    }
+}
