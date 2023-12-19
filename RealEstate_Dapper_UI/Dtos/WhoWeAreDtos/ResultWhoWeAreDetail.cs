53c883e2d2a57e2c0ba4eb68e798456b5c39cae0
ayse- <ayse-@DESKTOP-2S5TDQT>
2023-12-19 18:42:11 +0300

    AdminLayoutDuzenlemeleri

---
+++ RealEstate_Dapper_UI/Dtos/WhoWeAreDtos/ResultWhoWeAreDetail.cs
@@ -0,0 +1,6 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.WhoWeAreDtos
+{
+    public class ResultWhoWeAreDetail
+    {
+    }
+}
---
+++ RealEstate_Dapper_UI/Views/ViewComponents/AdminLayout/_AdminLayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.Views.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutSidebarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();  
+        }
+    }
+}
