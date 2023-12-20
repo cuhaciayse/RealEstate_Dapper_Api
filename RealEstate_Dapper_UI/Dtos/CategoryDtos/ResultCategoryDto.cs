589d4be3493221843ee7960e350641eeb313c427
ayse- <ayse-@DESKTOP-2S5TDQT>
2023-12-20 11:41:25 +0300

    Kategori ekleme yapıldı

--- RealEstate_Dapper_UI/Dtos/CategoryDtos/ResultCategoryDto.cs
+++ RealEstate_Dapper_UI/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -5,5 +5,13 @@
         public int CategoryID { get; set; }
         public string CategoryName { get; set; }
         public bool CategoryStatus { get; set; }
+
+        //public string CategoryStatusString
+        //{
+        //    get
+        //    {
+        //        return CategoryStatus ? "Pasif" : "Aktif";
+        //    }
+        //}
     }
 }
