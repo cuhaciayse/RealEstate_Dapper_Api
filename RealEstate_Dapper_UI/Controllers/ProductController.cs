bf816d85637451a7eb07e2e9509d697525669f84
ayse- <ayse-@DESKTOP-2S5TDQT>
2023-12-20 17:34:13 +0300

    Product controller uı

---
+++ RealEstate_Dapper_UI/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using RealEstate_Dapper_UI.Dtos.ProductDtos;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7124/api/Products/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> SearchByCity(string city)
+        //{
+        //    try
+        //    {
+        //        var client = _httpClientFactory.CreateClient();
+        //        var responseMessage = await client.GetAsync($"https://localhost:7124/api/Products/SearchByCity?city={Uri.EscapeDataString(city)}");
+
+        //        if (responseMessage.IsSuccessStatusCode)
+        //        {
+        //            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        //            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData, new JsonSerializerSettings
+        //            {
+        //                ContractResolver = new DefaultContractResolver
+        //                {
+        //                    NamingStrategy = new CamelCaseNamingStrategy()
+        //                }
+        //            });
+        //            return View("Index", values); // İlan listesi sayfasına dön
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Hata durumunda loglama veya işlem yapabilirsiniz.
+        //        Console.WriteLine(ex.Message);
+        //    }
+
+        //    return View("Index"); // Hata durumunda yine ilan listesi sayfasına dön
+        //}
+
+
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            return View();
+        }
+    }
+}
